3a1dd040de214a337982fa64ac5380a82b62cb05
Ilya <kuleshov.ia@phystech.edu>
2023-03-15 12:47:07 +0300

    add dvc

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ from pytorch_lightning.cli import LightningCLI
 from cifar_simclr_resnet.data import CIFARModule
 from cifar_simclr_resnet.model import ResNetSupervised
 
-LightningCLI(model_class=ResNetSupervised, datamodule_class=CIFARModule)
+cli = LightningCLI(model_class=ResNetSupervised, datamodule_class=CIFARModule, run=False, save_config_callback=None)
+cli.trainer.fit(model=cli.model, datamodule=cli.datamodule)
+cli.trainer.test(model=cli.model, datamodule=cli.datamodule)
